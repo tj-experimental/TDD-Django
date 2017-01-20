0991d066138eb0e47e638539c4eae5271ef4b7af
tonyejack <jtonye@ymail.somc>
2017-01-20 10:44:51 -0500

    Added a new app lists and modified functional test and unittest for the app

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome('./chromedriver')
 
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('./chromedriver')
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_run_app(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail('Finish the Test')
+
+
+
