93076465d12adc343feaedf79e36c0ca6752c8e6
Tonye Jack <jtonye@ymail.com>
2017-02-13 11:36:44 -0500

    Modified functional test

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -5,13 +5,19 @@ import sys, time
 
 class FunctionalTests(TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('./chromedriver.exe')
+        self.browser = webdriver.Chrome('../chromedriver.exe')
+        self.local_host = 'http://localhost:800'
         self.browser.implicitly_wait(3)
 
     def test_can_open_webpage(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.local_host)
         self.assertIn('Django', self.browser.title)
 
     def tearDown(self):
-        time.sleep(100)
+        # time.sleep(10)
         self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.local_host)
+
+
