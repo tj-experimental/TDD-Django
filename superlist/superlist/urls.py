f845d5548fc56854884db6393cba5ac420df674f
Tonye Jack <jtonye@ymail.com>
2017-02-13 18:41:27 -0500

    Modified test and added home.html

--- superlist/superlist/urls.py
+++ superlist/superlist/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from lists.views import home_page
+
 urlpatterns = [
+    url(r'^$', home_page, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
